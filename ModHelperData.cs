fb1d2e06975ebbe48f47a713a26bdc35ce7bd042
Stiefanek <107305979+Stiefanek@users.noreply.github.com>
2022-09-24 23:48:34 +0200

    Flamingo Tower

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+﻿namespace Flamingo
+{
+    public static class ModHelperData
+    {
+        public const string Version = "1.0.0";
+        public const string Name = "Flamingo";
+        public const string Description =
+            "Adds Flamingo";
+
+        public const string RepoOwner = "stiefanek.";
+        public const string RepoName = "Flamingo";
+        public const string Icon = "Flamenco";
+    }
+}
